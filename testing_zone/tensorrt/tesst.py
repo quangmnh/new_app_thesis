13e8485d35cf78ff4db3fec1b02b23741935df93
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-05-02 23:11:08 +0700

    xzcxzc

--- testing_zone/tensorrt/tesst.py
+++ testing_zone/tensorrt/tesst.py
@@ -2,6 +2,8 @@ from onnx_helper import ONNXClassifierWrapper
 import numpy as np
 PRECISION = np.float32
 BATCH_SIZE=32
-dummy_input_batch = np.zeros((BATCH_SIZE, 3, 48, 48))
+dummy_input_batch = np.zeros((BATCH_SIZE, 48, 48, 3))
 N_CLASSES = 5 # Our ResNet-50 is trained on a 1000 class ImageNet task
 trt_model = ONNXClassifierWrapper("new_model.trt", [BATCH_SIZE, N_CLASSES], target_dtype = PRECISION)
+predictions = trt_model.predict(dummy_input_batch)
+print(predictions)
