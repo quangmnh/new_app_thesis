9cad5b488db8cadcd7f34f460c34d0ad67248bf6
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-05-03 21:08:59 +0700

    Update main.py

--- testing_zone/tensorrt/main.py
+++ testing_zone/tensorrt/main.py
@@ -4,7 +4,7 @@ camera = CameraManagement()
 emo_model = ONNXClassifierWrapper("new_model.trt", [1, 5], target_dtype = np.float32)
 caffe_model = ONNXClassifierWrapper2("new_caffe.trt", [1, 1, 200, 7], 0.5, target_dtype = np.float32)
 
-
+print("ready")
 while True:
     frame = camera.get_frame()
     box = caffe_model.predict(camera.get_blob(frame))
