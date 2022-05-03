3f7824b3963a8ce761b0c8e479d54632c4efb204
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-05-03 18:24:22 +0700

    xczcx

--- testing_zone/tensorrt/comparing_new.py
+++ testing_zone/tensorrt/comparing_new.py
@@ -12,11 +12,8 @@ caffe_model = SSDCaffeModel(modelFile="./input/res10_300x300_ssd_iter_140000.caf
 data_path = "./input/data1"
 filepaths = []
 images = []
-res = []
-time_total = 0
-dummy_input = np.zeros((1, 48, 48, 1))
-for _ in range(10):
-    _ = trt_model.predict(dummy_input)
+res = 0
+time_total = 0.0
 for root, directories, files in os.walk(data_path):
     for filename in files:
         filepath = os.path.join(root, filename)
@@ -25,7 +22,9 @@ for root, directories, files in os.walk(data_path):
         label = filepath.split('/')[4]
         images.append({"label": label, "image": image})
         filepaths.append(filepath)
-
+dummy_input = np.zeros((1, 48, 48, 1))
+for _ in range(10):
+    _ = trt_model.predict(dummy_input)
 for image in images:
     frame = image["image"]
     # print("??????????????a")
@@ -34,10 +33,15 @@ for image in images:
     roi = get_roi(box, frame)
     # print("??????????????c")
     # print(roi.shape)
-    time_total += timeit('trt_model.predict(roi)')
+    # time_total += timeit('trt_model.predict(roi)')
+    start = time()
+    label = trt_model.predict(roi)
+    time_total+=time()-start
+    if label == image["label"]:
+        res+=1
     
     
     # print(emo_model.predict(roi))
 print("total time for tensorrt model")
-print(time_total)    
-
+print(time_total*1000)    
+print(res*1.0/len(images))
--- testing_zone/tensorrt/comparing_old.py
+++ testing_zone/tensorrt/comparing_old.py
@@ -1,12 +1,13 @@
 from model_manager import *
 import os
 from timeit import timeit
+from time import time
 # camera = CameraManagement()
 # trt_model = ONNXClassifierWrapper("new_model_fp16.trt", [1, 5], target_dtype = np.float16)
 emo_model = KerasEmotionClassificationModel("./input/facial_emotion_recognition_new_dataset.h5")
 caffe_model = SSDCaffeModel(modelFile="./input/res10_300x300_ssd_iter_140000.caffemodel",configFile="./input/deploy.prototxt.txt")
 # print("??????????????")
-
+res = 0.0
 data_path = "./input/data1"
 filepaths = []
 images = []
@@ -30,7 +31,16 @@ for image in images:
     # print(roi.shape)
     # print(trt_model.predict(roi)[0])
     # print(emo_model.predict(roi))
-    time_total+= timeit('emo_model.predict(roi)')
-print("total time for keras model")
-print(time_total)
+    start = time()
+    label = emo_model.predict(roi)
+    time_total+=time()-start
+    if label == image["label"]:
+        res+=1
+    
+    
+    # print(emo_model.predict(roi))
+print("total time for tensorrt model")
+print(time_total*1000)    
+print(res*1.0/len(images))
+
 
