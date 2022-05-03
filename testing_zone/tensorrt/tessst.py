2fd37a392f429efee3c08f139d9ea821c4448d87
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-05-03 13:18:30 +0700

    sdfsdf

---
+++ testing_zone/tensorrt/tessst.py
@@ -0,0 +1,18 @@
+from onnx_helper import ONNXClassifierWrapper
+import numpy as np
+import cv2
+
+frame = cv2.imread("./input/0.png")
+PRECISION = np.float16
+BATCH_SIZE=1
+# dummy_input_batch = np.zeros((BATCH_SIZE, 3, 300, 300))
+
+bruh = 1
+bruh2 = 1
+FACE_NUMBER = 200
+BOX = 7
+
+blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104., 177., 123.))
+trt_model = ONNXClassifierWrapper("new_cafe_fp16.trt",[bruh, bruh2, FACE_NUMBER, BOX] , target_dtype = PRECISION)
+predictions = trt_model.predict(blob)
+print(predictions)
