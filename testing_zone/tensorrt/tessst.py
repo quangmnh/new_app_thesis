84c269253231e6383fe5519c95b8b1f41f64edeb
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-05-03 13:58:08 +0700

    cvbvcb

--- testing_zone/tensorrt/tessst.py
+++ testing_zone/tensorrt/tessst.py
@@ -24,3 +24,17 @@ for i in range(0, predictions.shape[2]):
 
         if confidence > 0.5:
             print(predictions[0, 0, i, :])
+
+
+modelFile = './input/res10_300x300_ssd_iter_140000.caffemodel'
+configFile = './input/deploy.prototxt.txt'
+net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+net.setInput(blob)
+detections = net.forward()
+print("bbjdknbjsadnkkkkkkkk")
+for i in range(0, detections.shape[2]):
+
+    confidence = detections[0, 0, i, 2]
+
+    if confidence > 0.5:
+        print(detections[0, 0, i, :])
