7240ed19d32257f9c9fd64cbdf3e7ef7d22182b1
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-05-08 14:01:43 +0700

    dfaf

---
+++ testing_zone/tensorrt/comparing_caffe_.py
@@ -0,0 +1,40 @@
+from multiprocessing import dummy
+from timeit import timeit
+from model_manager import *
+import os
+from time import time
+# camera = CameraManagement()
+trt_model = ONNXClassifierWrapper("new_model.trt", [1, 5], target_dtype = np.float32)
+# emo_model = KerasEmotionClassificationModel("./input/facial_emotion_recognition_new_dataset.h5")
+caffe_model = SSDCaffeModel(modelFile="./input/res10_300x300_ssd_iter_140000.caffemodel",configFile="./input/deploy.prototxt.txt")
+# print("??????????????")
+
+data_path = "./input/data"
+filepaths = []
+images = []
+res = 0
+time_total = 0.0
+count =0
+for root, directories, files in os.walk(data_path):
+    for filename in files:
+        filepath = os.path.join(root, filename)
+        # print(filepath)
+        image = cv2.imread(filepath)
+        label = filepath.split('/')[4]
+        images.append({"label": label, "image": image})
+        filepaths.append(filepath)
+dummy_input = np.zeros((1, 48, 48, 1))
+for _ in range(10):
+    _ = trt_model.predict(dummy_input)
+for image in images:
+    frame = image["image"]
+    # print("??????????????a")
+    start = time()
+    box = caffe_model.get_boxes(frame=frame, blob=get_blob(frame))
+    time_total+=time()-start
+    count+=1
+    # print(emo_model.predict(roi))
+print("total time for tensorrt model")
+print(time_total*1000)    
+print(res*1.0/count)
+print(count)
---
+++ testing_zone/tensorrt/tessssst.py
@@ -0,0 +1,33 @@
+import caffe
+#load the model
+net = caffe.Net('models/bvlc_reference_caffenet/deploy.prototxt',
+                'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
+                caffe.TEST)
+
+# load input and configure preprocessing
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_mean('data', np.load('python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))
+transformer.set_transpose('data', (2,0,1))
+transformer.set_channel_swap('data', (2,1,0))
+transformer.set_raw_scale('data', 255.0)
+
+#note we can change the batch size on-the-fly
+#since we classify only one image, we change batch size from 10 to 1
+net.blobs['data'].reshape(1,3,227,227)
+
+#load the image in the data layer
+im = caffe.io.load_image('examples/images/cat.jpg')
+net.blobs['data'].data[...] = transformer.preprocess('data', im)
+
+#compute
+out = net.forward()
+
+# other possibility : out = net.forward_all(data=np.asarray([transformer.preprocess('data', im)]))
+
+#predicted predicted class
+print out['prob'].argmax()
+
+#print predicted labels
+labels = np.loadtxt("data/ilsvrc12/synset_words.txt", str, delimiter='\t')
+top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+print labels[top_k]
